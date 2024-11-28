9c4c1b06ebc2bdd3f271b3e396391c304b84b5cb
dolph <dolphinclan@outlook.com>
2024-11-28 19:23:36 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import artifactCover from './assets/image 22.svg'
+import {Artifact} from './Artifact.jsx'
+import background from './assets/MAMA_Background_with_Curves.svg'
+import more from './assets/more.svg'
+import next from './assets/next.svg'
+
+function App() {
+
+  return (
+      <div className="EndPage" style={{backgroundImage: `url(${background})`}}>
+          <h1 className={'titleChinese'}>自我认知部分观览结束</h1>
+          <h1 className={'titleEnglish'}>This is the end of the section</h1>
+          <h1 className={'sectionName'}>Self Awareness</h1>
+          <div className={'artifactTitleContainer'}>
+              <h2 className={'artifactTitle'}>荀子与《性恶论》</h2>
+          </div>
+          <div className={'artifact-container'}>
+              <img src={artifactCover} className={'artifactCover'}></img>
+          </div>
+          <button className={'button'}>
+              <img className={'more'} src={more}></img>
+          </button>
+          <button className={'button'}>
+              <img src={next} className={'next'}></img>
+          </button>
+      </div>
+  )
+}
+
+export default App
---
+++ src/Artifact.jsx
@@ -0,0 +1,10 @@
+import artifactCover from './assets/image 22.svg'
+export function Artifact() {
+    return (
+        <>
+            <div className="artifact-container">
+                <img src={artifactCover} className="artifact-cover"/>
+            </div>
+        </>
+    )
+}
